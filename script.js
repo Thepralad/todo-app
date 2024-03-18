bdfac8d3ce5ec6963ebf93be13a92f9ffd04e497
thepralad <prgurung04@gmail.com>
2024-03-18 23:56:52 +0530

    referencing all the required elements

---
+++ script.js
@@ -0,0 +1,10 @@
+const addBtn = document.querySelector('.add');
+
+addBtn.addEventListener('click', () =>{
+    const taskText = document.getElementById('field').value;
+    const task = document.createElement('p');
+    const doneBTn = document.createElement('button');
+    const tasksLi = document.createAttribute('li');
+    task = taskText;
+    
+})
