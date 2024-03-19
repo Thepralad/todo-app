4fbee539871d3d1aeee2bc7b3c7fc2547216e517
thepralad <prgurung04@gmail.com>
2024-03-19 20:58:07 +0530

    Implemented simple add and remove task button

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ const tasks = document.querySelector('.tasks');
 
 addBtn.addEventListener('click', () => {
     const taskText = document.getElementById('field').value;
+    document.getElementById('field').value = '';
     const taskLi = document.createElement('li'); // Create a <li> element for the task
     const taskParagraph = document.createElement('p'); // Create a <p> element for the task text
     taskParagraph.textContent = taskText; // Set the text content of the <p> element
     const doneBtn = document.createElement('button'); // Create a button for marking task as done
 
-    doneBtn.textContent = 'Done'; // Set button text
+    doneBtn.textContent = 'x'; // Set button text
     
-
+    if (taskParagraph.textContent == ''){
+        alert("Tasks cannot be empty!");
+        return 0;
+    }
     taskLi.appendChild(taskParagraph); // Append the <p> element to the <li>
     taskLi.appendChild(doneBtn); // Append the done button to the <li>
     tasks.appendChild(taskLi); // Append the <li> to the task list
